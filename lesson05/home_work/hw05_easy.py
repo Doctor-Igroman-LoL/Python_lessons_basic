1ef5230269763b436475f6924d94bc9b2f0a579e
Doctor_Igroman_LoL <dr.igroman.lol@gmail.com>
2019-02-08 17:55:12 +0900

    Урок 5. Решение 1 задачи (Нормал)

--- lesson05/home_work/hw05_easy.py
+++ lesson05/home_work/hw05_easy.py
@@ -6,33 +6,54 @@ import os
 import shutil       # Модуль работа с файлами
 import re
 
-print('-' * 5, 'Решение первой задачи', '-' * 5)
+def create(number=10):
+    try:
+        for n in range(number):
+            os.mkdir('dir_{}'.format(n))
+    except OSError:
+        print('Такие папки уже существуют')
 
-try:
-    for n in range(9):
-        os.mkdir('dir_{}'.format(n + 1))
-except OSError:
-    print('Такие папки уже существуют')
-
-input('Папки создались')
-
-try:
-    for n in range(9):
-        os.rmdir('dir_{}'.format(n + 1))
-except OSError:
-    print('Не существует таких папок')
-
-print('Папки удалились')
-
+def remove(number=10):
+    try:
+        for n in range(number):
+            os.rmdir('dir_{}'.format(n))
+    except OSError:
+        print('Не существует таких папок')
+#create()
+#remove()
 # Задача-2:
 # Напишите скрипт, отображающий папки текущей директории.
-print('-' * 5, 'Решение второй задачи', '-' * 5)
 
-print([dir_x for dir_x in os.listdir() if re.search('^[А-Яа-яA-Za-z0-9-_]*$', dir_x)])
+def list_dir():
+    print([dir_x for dir_x in os.listdir() if re.search('^[А-Яа-яA-Za-z0-9-_]*$', dir_x)])
 
+#list_dir()
 # Задача-3:
 # Напишите скрипт, создающий копию файла, из которого запущен данный скрипт.
 
-print('-' * 5, 'Решение третий задачи', '-' * 5)
+def copy_file(perfix):
+    shutil.copyfile('{}/hw05_easy.py'.format(os.getcwd()), '{}/hw05_easy_{}.py'.format(os.getcwd(), perfix))
 
-shutil.copyfile('{}/hw05_easy.py'.format(os.getcwd()), '{}/hw05_easy_copy.py'.format(os.getcwd()))
+#copy_file('Bug')
+# Функции
+
+def view_content(path='.'):
+    if not os.listdir(path) == []:
+        print(os.listdir(path))
+    else:
+        print('Нет содержимого')
+
+def go_to_folder(path, name_dir):
+    return os.path.join(path, name_dir)
+
+def remove_dir(path, name_dir):
+    os.rmdir('{}/{}'.format(path, name_dir))
+
+def create_dir(path, name_dir):
+    os.mkdir('{}/{}'.format(path, name_dir))
+
+def list_dir(path, name_dir):
+    for dir_x in os.listdir(path):
+        if re.search('^[А-Яа-яA-Za-z0-9-_]*$', dir_x):
+            if dir_x == name_dir:
+                return True
--- lesson05/home_work/hw05_normal.py
+++ lesson05/home_work/hw05_normal.py
@@ -13,3 +13,46 @@
 # Для решения данной задачи используйте алгоритмы из задания easy,
 # оформленные в виде соответствующих функций,
 # и импортированные в данный файл из easy.py
+import os
+
+from hw05_easy import go_to_folder
+from hw05_easy import view_content
+from hw05_easy import remove_dir
+from hw05_easy import list_dir
+from hw05_easy import create_dir
+
+current_dir = os.getcwd()
+
+if __name__ == '__main__':
+    while True:
+        try:
+            action = int(input('Введите команду: 1. Перейти в папку, 2. Просмотреть содержимое текущей папки, '
+                           '3. Удалить папку, 4. Создать папку, 5. Выход: '))
+            print('Введина команда: {}'.format(action))
+            if action == 1:
+                name_dir = input('Введите имя папки: ')
+                if list_dir(current_dir, name_dir):
+                    current_dir = go_to_folder(current_dir, name_dir)
+                    print('Вы успешно перешли по пути: {}'.format(current_dir))
+                else:
+                    print('Не возможно перейти')
+            elif action == 2:
+                view_content(current_dir)
+            elif action == 3:
+                try:
+                    name_dir = input('Введите имя папки, которую хотите удалить: ')
+                    remove_dir(current_dir, name_dir)
+                    print('Вы успешно удалили папку: {}'.format(name_dir))
+                except:
+                    print('Невозможно удалить. Нет такой папки')
+            elif action == 4:
+                try:
+                    name_dir = input('Введите имя папки, которую хотите создать: ')
+                    create_dir(current_dir, name_dir)
+                    print('Папка успешна создана: {}'.format(name_dir))
+                except:
+                    print('Не возможно создать папку')
+            elif action == 5:
+                break
+        except:
+            print('Вы вели не верную команду')
